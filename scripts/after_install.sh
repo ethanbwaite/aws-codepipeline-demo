fe41ce7e2550e5558141f9e306a0a1fe481d8264
Ethan Waite <43476516+ethanbwaite@users.noreply.github.com>
2021-10-30 14:59:55 -0700

    updating for npm and pm2 scripts

--- scripts/after_install.sh
+++ scripts/after_install.sh
@@ -1,4 +1,11 @@
 #!/bin/bash
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
+[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
+export HOME="/home/ubuntu/"
+sudo PM2_HOME=/home/ubuntu/.pm2 pm2 list
+cd /home/ubuntu/api
+npm install
 cd /home/ec2-user/server/build
 npm install
 npm install --save create-react-app react-scripts
