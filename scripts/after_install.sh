3fedf553a821e34b9657b4747e39de3724f066df
Ethan Waite <43476516+ethanbwaite@users.noreply.github.com>
2021-10-30 13:54:45 -0700

    changed serve to run in pm2 instance\

--- scripts/after_install.sh
+++ scripts/after_install.sh
@@ -3,3 +3,4 @@ cd /home/ec2-user/server
 npm install
 npm install --save create-react-app react-scripts
 npm install pm2 -g
+npm install -g serve
--- scripts/app_start.sh
+++ scripts/app_start.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
-cd /home/ec2-user/server
-npm install -g serve
-serve -s build
+cd /home/ec2-user/server/build
+pm2 --name demo-app serve -s build
 pm2 startup
 pm2 save
 pm2 restart all
