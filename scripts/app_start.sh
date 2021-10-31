59b7e61926fc2e3476d9101ff610d5daa56fd1e2
Ethan Waite <43476516+ethanbwaite@users.noreply.github.com>
2021-10-30 20:54:13 -0700

    pm2 changes

--- scripts/app_start.sh
+++ scripts/app_start.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 cd /home/ec2-user/server/build
 # sudo pkill -f PM2
-sudo pm2 stop all
+sudo pm2 delete all
 sudo pm2 start --name demo serve -s build
 # sudo pm2 startup
 # sudo pm2 save
