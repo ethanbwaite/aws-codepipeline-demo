cc5db7a15ed21b982e8d44a8c275004a5da03c25
Ethan Waite <43476516+ethanbwaite@users.noreply.github.com>
2021-11-07 11:19:34 -0800

    modify appstart

--- scripts/app_start.sh
+++ scripts/app_start.sh
@@ -1,13 +1,8 @@
 #!/bin/bash
 cd /home/ec2-user/server/build
-# sudo pkill -f PM2
+sudo pkill -f PM2
 sudo pm2 delete all
-pm2 delete all
 sudo pm2 start --name demo serve -s build
-pm2 start --name demo serve -s build
 sudo pm2 startup
-pm2 startup
 sudo pm2 save
-pm2 save
 sudo pm2 restart all
-pm2 restart all
